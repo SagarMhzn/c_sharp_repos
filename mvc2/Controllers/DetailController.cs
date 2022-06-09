2dd7a57717d10b42273d3fbf3cc48e5f4c5af290
SagarMhzn <maharzansagar4u@gmail.com>
2022-06-09 12:35:56 +0545

    mvc updated

--- mvc2/Controllers/DetailController.cs
+++ mvc2/Controllers/DetailController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using mvc2.Models;
+
 
 namespace mvc2.Controllers
 {
@@ -11,7 +13,18 @@ namespace mvc2.Controllers
         // GET: Detail
         public ActionResult displayDetail()
         {
-            return View();
+            var res = GetDetail();
+            return View(res);
+        }
+
+        private subject GetDetail()
+        {
+            return new subject()
+            {
+                Id = 101,
+                subjectName = "Ncc"
+
+            };
         }
 
         // creating own action method
---
+++ mvc2/Models/subject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc2.Models
+{
+    public class subject
+    {
+        public int Id { get; set; }
+        public string subjectName { get; set; }
+    }
+}
